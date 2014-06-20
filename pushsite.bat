9799a235756046c0707c0bd7864001e056b9ab7f
brian.paulson@issinc.com <brian.paulson@issinc.com>
2014-06-20 13:05:07 -0600

    Added Bat script for windows

---
+++ pushsite.bat
@@ -0,0 +1,13 @@
+cd c:\opt\brianpaulson.com
+
+hugo
+
+xcopy /s C:\opt\brianpaulson.com\public C:\opt\spectre013.github.io
+
+cd C:\opt\spectre013.github.io
+
+git add -A
+
+git commit -m "Blog Update"
+
+git push
